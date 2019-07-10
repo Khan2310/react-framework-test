86d33d1b0eaff402a6a9a08c43cc72c2d2261b81
Touhid Mia <touhid@bitmascot.com>
2019-07-10 09:57:35 +0600

    UI System

--- applications/ui-system/theme/theme.stories.tsx
+++ applications/ui-system/theme/theme.stories.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import BismillahTheme from './bismillah-theme';
 
-
 storiesOf('Theme', module).add('Bismillah Theme', () => <BismillahTheme/>);
 
 
